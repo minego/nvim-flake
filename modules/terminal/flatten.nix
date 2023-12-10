6c4de686a5f8da9846ef0351bd07c93bb187b629
Micah N Gorrell <m@minego.net>
2023-12-10 09:22:53 -0700

    Tweaked options for flatten.nvim to handle man pages, and to use a vsplit since using alternate usually resulted in me losing my terminal when closing the opened file

--- modules/terminal/flatten.nix
+++ modules/terminal/flatten.nix
@@ -15,11 +15,21 @@ in {
 		];
 
 		vim.luaConfigRC = ''
+			-- Open man pages in neovim splits as well
+			vim.cmd('let $MANPAGER="nvim +Man!"')  
+
             require("flatten").setup({
             	window = {
-            		open = "alternate",
+					-- I prefer using 'alternate' but then closing the opened
+					-- window causes the whole tab to close...
+					--
+					-- Restore this option once I figure out a way to prevent
+					-- that happening...
+            		-- open = "alternate",
+
+            		open = "vsplit",
             		focus = "first",
-            	}
+            	},
             })
             '';
 	});
