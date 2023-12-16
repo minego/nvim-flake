c84bef40022ee2878f5a20c5f2bea792ba2344e6
Micah N Gorrell <m@minego.net>
2023-12-16 11:19:45 -0700

    Turned on missing configuration item

--- modules/build/build.nix
+++ modules/build/build.nix
@@ -25,7 +25,7 @@ in {
             require("cmake-tools").setup {
             	cmake_regenerate_on_save			= true,
             
-            	cmake_generate_options				= { "-G", "Ninja" },
+            	cmake_generate_options				= { "-G", "Ninja", "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
             	cmake_build_options					= {},
             
             	cmake_build_directory				= "build/''${variant:buildType}",
